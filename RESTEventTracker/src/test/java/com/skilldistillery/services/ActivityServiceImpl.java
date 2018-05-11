1b205537641666ea94a51c29a5415f1a6fb06e9c
Rebecca Allen <rebeccazarsky@gmail.com>
2018-05-11 17:22:34 -0600

    stub out rest

---
+++ RESTEventTracker/src/test/java/com/skilldistillery/services/ActivityServiceImpl.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityServiceImpl implements ActivityService{
+
+}
---
+++ RESTEventTracker/src/test/java/com/skilldistillery/services/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService{
+
+}
