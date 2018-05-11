919753ca171d46f7c604e99c4b92b158fd45511a
Rebecca Allen <rebeccazarsky@gmail.com>
2018-05-11 16:42:28 -0600

    add entities

--- JPAEventTracker/src/com/skilldistillery/entities/Category.java
+++ JPAEventTracker/src/com/skilldistillery/entities/Category.java
@@ -1,9 +1,16 @@
 package com.skilldistillery.entities;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Category {
@@ -11,5 +18,81 @@ public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="start_time")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date startTime;
+	
+	@Column(name="end_time")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date endTime;
+	
+	@ManyToOne
+	@JoinColumn(name="category_id")
+	private Category category;
+	
+	// End of Fields
 
+	public Category() {
+		super();
+	}
+	
+	// End of Constructors
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	// End of Gets/Sets
+	
 }
